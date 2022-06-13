edfd64312dcd5c5e4b299fd8538b0ec35ee41e9c
Alberto Leal <mail4alberto@gmail.com>
2022-06-13 17:59:46 -0400

    add more middleware asserts whenever system.base-hostname is omitted

--- tests/sentry/middleware/test_subdomain.py
+++ tests/sentry/middleware/test_subdomain.py
@@ -23,3 +23,10 @@ class SubdomainMiddlewareTest(TestCase):
             assert request_with_host("us.dev.getsentry.net:8000").subdomain is None
             assert request_with_host("foobar.us.dev.getsentry.net:8000").subdomain == "foobar"
             assert request_with_host("foo.bar.us.dev.getsentry.net:8000").subdomain == "foo.bar"
+
+        with self.options({}):
+            assert request_with_host("foobar").subdomain is None
+            assert request_with_host("dev.getsentry.net:8000").subdomain is None
+            assert request_with_host("us.dev.getsentry.net:8000").subdomain is None
+            assert request_with_host("foobar.us.dev.getsentry.net:8000").subdomain is None
+            assert request_with_host("foo.bar.us.dev.getsentry.net:8000").subdomain is None
